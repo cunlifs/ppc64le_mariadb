73a6799526dc2751527ccfeaf4b064d07a96f24a
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2021-08-12 13:55:24 +0100

    Update new_db.sql

--- new_db.sql
+++ new_db.sql
@@ -11,20 +11,13 @@ released INTEGER
 );
 
 INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("Bohemian Rhapsody", "Queen", "epic", 60, 1975);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("Let it go", "Idina Menzel", "epic", 227, 2013);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("I will survive", "Gloria Gaynor", "epic", 198, 1978);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("Twist and Shout", "The Beatles", "happy", 152, 1963);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("La Bamba", "Ritchie Valens", "happy", 166, 1958);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("I will always love you", "Whitney Houston", "epic", 273, 1992);
-INSERT INTO songs (title, artist, mood, duration, released)
-    VALUES ("Sweet Caroline", "Neil Diamond", "happy", 201, 1969);
-INSERT INTO songs (title, artist, mood, duration, released)
+    VALUES ("Bohemian Rhapsody", "Queen", "epic", 60, 1975),
+    VALUES ("Let it go", "Idina Menzel", "epic", 227, 2013),
+    VALUES ("I will survive", "Gloria Gaynor", "epic", 198, 1978),
+    VALUES ("Twist and Shout", "The Beatles", "happy", 152, 1963),
+    VALUES ("La Bamba", "Ritchie Valens", "happy", 166, 1958),
+    VALUES ("I will always love you", "Whitney Houston", "epic", 273, 1992),
+    VALUES ("Sweet Caroline", "Neil Diamond", "happy", 201, 1969),
     VALUES ("Call me maybe", "Carly Rae Jepsen", "happy", 193, 2011);
     
-SELECT * FROM songs;
+#SELECT * FROM songs;
